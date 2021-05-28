42df8f54ac4847ec88999cb9d19662a360e454e3
FX24 <muhammad.farhan.fm00@gmail.com>
2021-05-28 13:10:22 +0700

    Push library

---
+++ app/src/main/java/com/dicoding/moviecatalogue/data/source/local/entity/TvShowEntity.kt
@@ -0,0 +1,36 @@
+package com.dicoding.moviecatalogue.data.source.local.entity
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.dicoding.moviecatalogue.data.source.remote.response.GenresItem
+
+@Entity(tableName = "tvshowentity")
+data class TvShowEntity(
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "id")
+    val id: Int,
+
+    @ColumnInfo(name = "title")
+    val title: String,
+
+    @ColumnInfo(name = "poster")
+    val poster: String,
+
+    @ColumnInfo(name = "release_date")
+    val release_date: String,
+
+    @ColumnInfo(name = "favorite")
+    var favorite: Boolean = false,
+
+    @ColumnInfo(name = "genre")
+    val genre: List<GenresItem>? = null,
+
+    @ColumnInfo(name = "overview")
+    val overview: String = "",
+
+    @ColumnInfo(name = "rating")
+    var rating: String = ""
+)
